ce951a49370d48db6bc8325a39c5d6bf9c5383e7
Claudio Famiglietti <claudio.famiglietti@gmail.com>
2022-02-19 22:27:22 +0100

    Navbar + basic style

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,13 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
+
   def new
     @bookmark = Bookmark.new
-    @list = List.find(params[:list_id])
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @list = List.find(params[:list_id])
-    # raise
     @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list)
@@ -17,13 +17,21 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    @bookmark = Bookmark.find(bookmark_params)
-    @bookmar
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
   end
 
   private
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
-  resources :lists, only: %i[new show create destroy] do
+  resources :lists, except: %i[edit update] do
     resources :bookmarks, only: %i[new create]
   end
-  resources :bookmarks, only: [:destroy]
+  resources :bookmarks, only: :destroy
 end
